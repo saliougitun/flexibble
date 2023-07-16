134169000f2ad8e59a23672214b32f37cf666ba0
unknown <lodia159@gmail.com>
2023-07-16 19:29:36 +0100

    first-commit

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,23 +1,24 @@
-import { g, auth, config } from '@grafbase/sdk'
+import { g, auth, config } from "@grafbase/sdk";
+import { url } from "inspector";
 
-// @ts-ignore
-const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 100 }),
+const User = g.model("User", {
+  name: g.string().length({ min: 2, max: 20 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
-  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  description: g.string().optional(),
   githubUrl: g.url().optional(),
-  linkedInUrl: g.url().optional(), 
+  linkedInUrl: g.url().optional(),
   projects: g.relation(() => Project).list().optional(),
-})
-
-// @ts-ignore
-const Project = g.model('Project', {
+});
+const Project = g.model("Project", {
   title: g.string().length({ min: 3 }),
-  description: g.string(), 
+  description: g.string(),
   image: g.url(),
-  liveSiteUrl: g.url(), 
-  githubUrl: g.url(), 
+  liveSiteUrl: g.url(),
   category: g.string().search(),
   createdBy: g.relation(() => User),
-})
+});
+
+export default config({
+  schema: g,
+});
