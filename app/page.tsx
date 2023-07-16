b260be1cf9c082447c7d254deeaf8d7d8f749602
unknown <lodia159@gmail.com>
2023-07-16 16:01:45 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <section className="flex-col flex-start paddings mb-6 ">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>loadMore</h1>
+    </section>
+  );
+};
+export default Home;
---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,28 @@
+import { g, auth, config } from '@grafbase/sdk'
+
+// @ts-ignore
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 100 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(), 
+  projects: g.relation(() => Project).list().optional(),
+}).auth((rules) => {
+  rules.public().read()
+})
+
+// @ts-ignore
+const Project = g.model('Project', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(), 
+  image: g.url(),
+  liveSiteUrl: g.url(), 
+  githubUrl: g.url(), 
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
+}).auth((rules) => {
+  rules.public().read()
+  rules.private().create().delete().update()
+})
